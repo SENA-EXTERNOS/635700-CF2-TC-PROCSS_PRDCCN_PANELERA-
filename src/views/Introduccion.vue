96b6bf6601af456c30b3b654896b761b1325162c
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-10-05 22:38:10 -0500

    Introducción
    Introducción OK

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,6 +8,25 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/introduccion/img-1.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Apreciado aprendiz, sea bienvenido a este ciclo educativo enfocado en los efectos de la temperatura en la producción panelera.
+        <br/>
+        <br/>
+        p.mb-5 Antes de empezar, se sugiere revisar el siguiente video donde podrá identificar el contexto de este componente formativo.
+        <br/>
+        <br/>
+        p.mb-3 <b>¡Éxitos en este proceso de aprendizaje!</b>
+
+    .row.mt-5
+      .col-12
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
