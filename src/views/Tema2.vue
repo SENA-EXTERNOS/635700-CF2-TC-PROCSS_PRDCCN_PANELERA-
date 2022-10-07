822af2541da52750344d3b01c60c8bc702e2dee1
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-10-07 00:45:22 -0500

    Tema 2
    Se inicia maquetación de tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,8 +5,15 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Procesos de transferencia de calor en la hornilla panelera 
 
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(data-aos='fade-right')(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/T2/img-1.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 La panela es un edulcorante altamente nutritivo que se elabora concentrando todos los sólidos solubles en agua y en el jugo de caña de azúcar. La panela se diferencia de la azúcar refinada en que contiene, además de sacarosa, glucosa y fructosa, una presencia importante de minerales, grasas, compuestos proteicos y vitaminas, desde el punto de vista nutricional.
+    p.mb-4 <b>A continuación, se presentan las fases del proceso de producción de panela.</b>
 </template>
 
 <script>
